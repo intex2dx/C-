06785450c93ee8598c73e29fba1140181123679f
intex2dx <112858282+intex2dx@users.noreply.github.com>
2023-02-04 16:25:41 +0300

    Create C.cpp

---
+++ week01/C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n=0, m=0;
+    cin>>n;
+    m = 1;
+    for (int i=1; i<=n;i++) m *= i;
+    cout<<m<<endl;
+    return 0;
+}
