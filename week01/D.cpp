f50ee337e403e64a4fa4615152a31ce8814b54cc
intex2dx <112858282+intex2dx@users.noreply.github.com>
2023-02-04 16:36:52 +0300

    Create D.cpp

---
+++ week01/D.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m=0, n=0;
+    cin>>m>>n;
+    int a = min(m,n), b = max(m, n), r=0;
+    while (b%a != 0){
+        r = b%a;
+        b = a;
+        a = r;
+    }
+    cout << a << endl;
+}
