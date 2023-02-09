2551fb57b3f04857848ca3c4f65795451062186b
intex2dx <112858282+intex2dx@users.noreply.github.com>
2023-02-09 12:08:00 +0300

    Update B.cpp

--- week01/B.cpp
+++ week01/B.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main(){
     int a=0, b=0, c=0;
     cin>>a>>b>>c;
+    if (a!=0) {
     int D = b*b-4*a*c;
     if (D<0) cout<<endl;
     else if (D==0) cout<<(-b/(2*a))<<endl;
-    else cout<<((-b-sqrt(D))/(2*a))<<' '<<((-b+sqrt(D))/(2*a))<<endl;
+    else cout<<((-b-sqrt(D))/(2*a))<<' '<<((-b+sqrt(D))/(2*a))<<endl;}
+    else if (b!=0) {
+        cout << -c/b << endl;
+    }
+    else {
+        cout << endl;
+    }
     return 0;
 }
