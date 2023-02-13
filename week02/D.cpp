6de14935ac24e28ab9fd5b82abed9494b7b05a64
intex2dx <artemov.ivan.2004@yandex.ru>
2023-02-13 09:52:49 +0300

    fixing

--- week02/D.cpp
+++ week02/D.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int recursive_power(int base, int power){
+long int recursive_power(int base, int power){
     if (power == 1) return base;
     else if (power == 0) return 1;
     else return base * recursive_power(base, power - 1);
