6813a1b7bc1c4c47ba8d6971ba4f0f8ddde67586
intex2dx <artemov.ivan.2004@yandex.ru>
2023-02-18 01:35:38 +0300

    0

--- week03/D.cpp
+++ week03/D.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #ifndef N
-#define N 4  // actual size of the array
+#define N 5
 #endif
 
 void swap(int& lha, int&rha){
@@ -13,13 +13,34 @@ void swap(int& lha, int&rha){
 using namespace std;
 int main() {
     int a[N] = {0};
-    for (int i=0;i<N;i++){   
-        cin >> a[i];}
-
-    for (int i=0; i<N; i++){
-        if (a[i] < 0 and a[i+1] > 0)
+    int b[N] = {0};
+    int i_a = 0, i_b = 0;
+    for (int i = 0; i < N; i++){
+        int elem;
+        cin >> elem;
+        if (elem > 0) {a[i_a] = elem; i_a++;}
+        else {b[i_b] = elem; i_b++;}
     }
-
-
+    if (a[0] != 0) {
+        cout << a[0];
+        int i = 1;
+        while (a[i] != 0 & i < N) {
+            cout << ' ' << a[i];
+            i += 1;
+        }
+        if (b[0] !=0) {
+          cout << '|';
+        }
+    }
+    if (b[0] != 0){
+        cout << b[0];
+        int i = 1;
+        while (b[i] !=0 & i < N){
+            cout << ' ' << b[i];
+            i += 1;
+        }
+    cout << endl;
+    }
+    
     return 0;
 }
