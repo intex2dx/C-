4bac62779bb0848d20219bfdacdfd7a88dc9d93a
intex2dx <artemov.ivan.2004@yandex.ru>
2023-02-26 22:30:46 +0300

    change B

--- week03/B.cpp
+++ week03/B.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 #ifndef N
-#define N 4  // actual size of the array
+#define N 2  // actual size of the array
 #endif
 
 void swap(int& lha, int&rha){
-    int cmp = rha;
+    int tmp = rha;
     rha = lha;
-    lha = cmp;
+    lha = tmp;
 }
 
 using namespace std;
@@ -15,7 +15,7 @@ int main() {
     int a[N] = {0};
     for (int i=0;i<N;i++){   
         cin >> a[i];}
-    for (int i=1; i<N; i++){
+    for (int i=N-1; i!=0; i--){
         swap(a[i-1], a[i]);
     }
     cout << a[0];
